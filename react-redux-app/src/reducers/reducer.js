090b84ba9eb64825b872eb14895c21bb9257facb
Angela Williams <lambda_school_loaner_97@Lambda-School-Loaner-97s-MacBook-Pro.local>
2020-03-03 15:36:35 -0700

    Added redux dependencies, action and reducer files

---
+++ react-redux-app/src/reducers/reducer.js
@@ -0,0 +1,14 @@
+import {
+FETCHING_QUOTE_START,
+FETCHING_QUOTE_SUCCESS, 
+FETCHING_QUOTE_FAILURE
+} from '../reducers/actions';
+
+const initialState = {};
+
+export const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+        return state;
+    }
+};
